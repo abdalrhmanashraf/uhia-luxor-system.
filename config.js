c851f1bb48287022d9031505077d0d70508b7608
abdalrhmanashraf <abdalrhmanashraf01@gmail.com>
2026-05-30 13:57:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,11 @@
 // ==========================================
 
 // الرابط الجديد بعد التحديث
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz_Z-lvfMPKOGcIRPvkvjD3eXnLl3ymX3G-s107B6cMO_daGnFc11TdfOp7FfqQJKg/exec';
+var SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz_Z-lvfMPKOGcIRPvkvjD3eXnLl3ymX3G-s107B6cMO_daGnFc11TdfOp7FfqQJKg/exec';
 
-// تصدير للوصول من باقي الملفات إذا لزم الأمر (לبيئات العمل الحديثة)
+// لضمان الوصول للرابط في كل المتصفحات
+window.SCRIPT_URL = SCRIPT_URL;
+
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = { SCRIPT_URL };
+  module.exports = { SCRIPT_URL: SCRIPT_URL };
 }
