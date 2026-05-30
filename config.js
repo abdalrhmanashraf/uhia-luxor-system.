ccb71b81255ed3ff963886124fa553908bfd1fa7
abdalrhmanashraf <abdalrhmanashraf01@gmail.com>
2026-05-30 05:10:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SCRIPT_URL  = 'https://script.google.com/macros/s/AKfycbxrc-PfVQKdVc1S4HoUS9Kj1hSt2iBKRaxxA6VKCRBAILKahq3cgXENy8uVOJUAbxJl5g';
-const BRANCH_NAME = 'فرع الأقصر';
-const LOGO_URL    = '';
+var SCRIPT_URL  = 'https://script.google.com/macros/s/AKfycbxrc-PfVQKdVc1S4HoUS9Kj1hSt2iBKRaxxA6VKCRBAILKahq3cgXENy8uVOJUAbxJl5g/exec';
+var BRANCH_NAME = 'فرع الأقصر';
+var LOGO_URL    = '';
