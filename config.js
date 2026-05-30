b8c6baf58bdb33a372cf2ff2ef84123700f0b534
abdalrhmanashraf <abdalrhmanashraf01@gmail.com>
2026-05-30 05:07:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SCRIPT_URL  = 'https://script.google.com/macros/s/AKfycbxrc-PfVQKdVc1S4HoUS9Kj1hSt2iBKRaxxA6VKCRBAILKahq3cgXENy8uVOJUAbxJl5g/exec';
+const SCRIPT_URL  = 'https://script.google.com/macros/s/AKfycbxrc-PfVQKdVc1S4HoUS9Kj1hSt2iBKRaxxA6VKCRBAILKahq3cgXENy8uVOJUAbxJl5g';
 const BRANCH_NAME = 'فرع الأقصر';
 const LOGO_URL    = '';
